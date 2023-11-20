4583e1279fb32aa3bd41362a005478a8869b983c
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2023-11-20 18:39:27 -0500

    Create Debug.java

---
+++ Debug.java
@@ -0,0 +1,25 @@
+
+
+
+//always start with importing our scanner so we can use it!
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //I've already set up a scanner with the name 'sc' for you!
+        Scanner sc = new Scanner(System.in);
+
+        //here's a good working example for you to reference!
+        System.out.println("What is your name? ");
+        String name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
+
+
+        //finish this one for me by printing the user's age back to them
+        System.out.println("How old are you? ");
+
+
+        }
+    }
+
+
