52c8ab8611def633733549d6d12f18e8c74fc16f
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2023-12-13 14:29:54 -0500

    HW1

---
+++ Homework 1/Debug.java
@@ -0,0 +1,40 @@
+//always start with importing our scanner so we can use it!
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //I've already set up a scanner with the name 'sc' for you!
+        Scanner sc = new Scanner(System.in);
+
+        //here's a good working example for you to reference!
+        //first: we ask our user for our prompt
+        System.out.println("What is your name? ");
+        //second: we create a string variable and store the user input back to it
+        String name = sc.nextLine();
+        //third: we print it back out!
+        System.out.println("Hello, " + name + "! ");
+
+
+        //finish this one for me by receiving and printing the user's age back to them
+        System.out.println("How old are you? ");
+
+
+        //can you do some math for me? take these variable and print out the sum of them!
+        Integer num1 = 10;
+        Integer num2 = 37;
+        //?
+
+        
+
+        
+        /*harder question: create a 1 question quiz game as in, ask a question 
+        and compare the response to what the correct answer should be. print out
+        an appropriate response based on whether it was right or wrong
+        i'm expecting an if/else statement likely, google if/else in java!
+        */
+        
+
+        }
+    }
+
+
