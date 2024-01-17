199fdf26edd189b20690d5f53320df04ed6fb229
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2024-01-17 12:20:55 -0500

    Create ArrayDebug.java

---
+++ unsorted/ArrayDebug.java
@@ -0,0 +1,30 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+//Our cheaty array helper file <3
+public class Main {
+    public static void main(String[] args) {
+        //Here's our first array!
+        String[] sortMe = {"Apple","Kiwi","Orange","Banana","Watermelon","Strawberry"};
+       //What method is missing here to actually sort our array?
+        Arrays.?(sortMe);
+        //And what method is missing here to print out that array?
+        System.out.println(?);
+
+
+        //another one!
+        int addMe[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        //How would we add every index together and print out the sum??
+        //A small hint:
+        int sum = 0;
+        for(?;?;?){
+
+        }
+        
+        //thinking one! here's an array
+        int maxMe[] = {280, 4892, 134, 23, 1937, 5329, 932, 89};
+        //What would you have to do to find the largest number and then print that value?
+        
+
+    }
+}
