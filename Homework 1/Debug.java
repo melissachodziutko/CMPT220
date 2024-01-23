d83b34f33c61082dfbf34cabfb2f8d652b2043f6
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2024-01-22 19:43:21 -0500

    Update Debug.java

--- Homework 1/Debug.java
+++ Homework 1/Debug.java
@@ -20,21 +20,13 @@ public class Main {
 
 
         //can you do some math for me? take these variable and print out the sum of them!
-        Integer num1 = 10;
-        Integer num2 = 37;
+        int num1 = 10;
+        int num2 = 37;
         //?
 
         
 
-        
-        /*harder question: create a 1 question quiz game as in, ask a question 
-        and compare the response to what the correct answer should be. print out
-        an appropriate response based on whether it was right or wrong
-        i'm expecting an if/else statement likely, google if/else in java!
-        */
-        
-
-        }
     }
+}
 
 
