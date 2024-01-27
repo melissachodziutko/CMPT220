4d6a6b2006e748353da8eb3bcac5180664b081b8
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2024-01-27 12:34:24 -0500

    Create ifelsepractice.java

---
+++ Homework 2/ifelsepractice.java
@@ -0,0 +1,19 @@
+//always start with importing our scanner so we can use it!
+import java.util.Scanner;
+
+
+/* our first practice file!
+* create a 3 question quiz game (lots of if/else likely)
+* requirements: keep track of the user's score, has to have at least 3 questions, use if/else
+* can be any topic you pick :) feel free to pick some obscure or niche topics!
+* good luck!
+* */
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
+
+
