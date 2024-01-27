cd8e2a7478bae3e12d2d2691dac1fecc87103ae6
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2024-01-27 11:24:07 -0500

    Update and rename Debug.py to Debug.java

---
+++ Homework 2/Debug.java
@@ -0,0 +1,44 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        //for this section: are these all printing the best option? If they aren't, fix it!
+        //(However you interpret 'fix' is fine i promise, any way you fix it shows you get the concept to me)
+        int var1 = 4;
+        if (var1 > 4){
+            System.out.println("Var1 is greater than 4");
+        }else{
+            System.out.println("Var1 is less than 4");
+        }
+
+        int var2 = 6;
+        if (var2 == 5){
+            System.out.println("Var2 is 5");
+        } else if (var2 > 5){
+            System.out.println("Var2 is greater than 5");
+        } else if (var2 < 5){
+            System.out.println("Var2 is less than 5");
+        } else{
+            System.out.println("Var2 is 6");
+        }
+
+        int var3 = 5;
+        if (var3 > 10){
+            System.out.println("Var3 is greater than 10");
+        } else {
+            System.out.println("Var3 is less than 10");
+        }
+
+
+
+
+        //for this section: why are we not entering the if statement?
+        if ("Marist" == "marist"){
+            System.out.println("Marist college!");
+        } else{
+            System.out.println("Not marist college :(!");
+        }
+
+
+    }
+}
