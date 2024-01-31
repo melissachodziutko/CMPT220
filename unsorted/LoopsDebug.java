f149606402febd26881a5755e55c442ee8b059e6
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2024-01-31 13:03:06 -0500

    Update LoopsDebug.java

--- unsorted/LoopsDebug.java
+++ unsorted/LoopsDebug.java
@@ -1,7 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-import java.util.Arrays;
-//Our cheaty array helper file <3
+
 public class Main {
     public static void main(String[] args) {
 
