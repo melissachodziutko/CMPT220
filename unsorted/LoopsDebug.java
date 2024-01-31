86e5d539ec37390f4ffdb6f588813fb9c6589e72
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2024-01-31 12:56:41 -0500

    Create LoopsDebug.java

---
+++ unsorted/LoopsDebug.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+//Our cheaty array helper file <3
+public class Main {
+    public static void main(String[] args) {
+
+
+        //P1: This one only prints 0-9, can you fix it so it prints 1-10?
+        System.out.println("Problem 1");
+        for (int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
+
+        //P2: Ask the user for a number. Create a loop to find the factorial of it
+        //(factorial = X!, X being the user input, Factorials are every digit before X multiplied together)
+        System.out.println("Problem 2");
+        System.out.println("Enter a number and I will tell you the fatorial: ");
+        //here's a hint
+        for (int i = 0; i < ?; i++){
+
+        }
+
+        //P3: Ask the user for a number, and then add together every OTHER digit (starting from 1)
+        System.out.println("Problem 3");
+        System.out.println("Enter a number and I will tell you the sum of every other number: ");
+        //No hint! what do you need to complete this task?
+
+
+        //P4: Why does this loop never stop!
+        //what can you do to break out of the loop after it prints once?
+        System.out.println("Problem 4");
+        boolean run = true;
+        while (run == true){
+            System.out.println("I printed once!");
+        }
+
+        //P5: 
+    }
+}
