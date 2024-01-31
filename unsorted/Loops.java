57185bd8d4ecd1d4cf90f9f979b36d658d874303
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2024-01-31 13:08:04 -0500

    Create Loops.java

---
+++ unsorted/Loops.java
@@ -0,0 +1,18 @@
+/*
+challenge file!
+this one isn't too tough but it does require a little bit of writing and some googling
+figure out how to take a string from the user
+then print back out every individual letter one letter per line
+I also am requiring a small write up: explain your discovery to me.
+how did you figure out how to do this? can you translate your code into simple terms? 
+you need to explain why you picked your for loop conditional and what's doing the work with the string
+if you're confused reach out!
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
