88d4c6cdff919b61575ebd63c066924d8f2470d3
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2024-02-12 10:05:15 -0500

    Update and rename ExtraPractice.java to ExtraLoopPractice.java

---
+++ unsorted/Homework 3/ExtraLoopPractice.java
@@ -0,0 +1,14 @@
+//https://www.w3resource.com/c-programming-exercises/for-loop/c-for-loop-exercises-6.php
+
+/*
+The above link has the problem + solution for C, which is similar to java, so it should be a hint if you're struggling. but, complete that problem in Java with a while loop
+(your print cannot look like the C solution, organize it differently)
+*/
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
