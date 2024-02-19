9c2555ee7d5462a0dda56f38640e250f19130ce8
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2024-02-19 10:48:19 -0500

    Update Array.java

--- Homework 4/Array.java
+++ Homework 4/Array.java
@@ -1,1 +1,16 @@
-g
+
+/*
+Take the given array and keep track of:
+1. Any duplicate numbers
+2. How many times the duplicate numbers appear
+3. Print out those duplicate numbers with how many times they appear
+
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        //Don't let the size of this scare you! no matter how big an array is, it all works the same!
+        int[] myArray = {10,3,295,38,20,3,4,267,2445,10,5566,87,93,17,10,2,87,267,3176,3,82};
+        //you cannot use the array util. Do this one by hand :(
+    }
+}
