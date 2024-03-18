abb9df28faeb84ff8cada19e187b441f636165c4
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2024-03-18 09:22:57 -0400

    Create functions.java

---
+++ Homework 5/functions.java
@@ -0,0 +1,13 @@
+/*
+for this one, if you remember, I asked you to come up with a 'reason' for a function and implement it. 
+Now you get to do it! yay!!!!!
+please go a little more complex than just simple math/simple printing for the function
+feel free to google some inspiration though :)
+*/
+
+public class Main {
+    public static void main(String[] args) {
+    
+    }
+   
+}
