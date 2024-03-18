ff0d9a2613f533d594745d8114694355bcd611c1
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2024-03-18 09:17:12 -0400

    Update and rename debug.java to debugFunctions.java

---
+++ Homework 5/debugFunctions.java
@@ -0,0 +1,43 @@
+/*
+Referencing your homework from https://github.com/melissachodziutko/CMPT220/blob/main/Homework%203/LoopsDebug.java
+Can you take problems 2, 3, and 5 and turn them into methods, and then call those methods in main?
+Things that may trip you up while working on this:
+1. the code works the same whether it's in a method or not, it just needs some minor variable adjustments probably
+2. be wary of opening and closing brackets!! 
+3. methods can't go inside methods!
+4. methods go outside public static main, but inside public class main!
+5. do you need to return the value? if so, make sure it isn't void
+
+I give you two fully working method examples with and without parameters and called them for your reference :)
+*/
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+
+    System.out.println("Enter your first number to be added");
+    int firstInput = sc.nextInt();
+    System.out.println("Enter your second number to be added");
+    int secondInput = sc.nextInt();
+    //remember this method is returning, so we need to print it out
+    System.out.println(add(firstInput,secondInput));
+
+    System.out.println("im going to print out the word Dog now.");
+    printDog();
+      
+    }
+
+  
+    public static int add(int zyx, int tuv){
+        return(zyx + tuv);
+    }
+
+  
+    public static void printDog(){
+        System.out.println("Dog");
+    }
+
+  
+}
