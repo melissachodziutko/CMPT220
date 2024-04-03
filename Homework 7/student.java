f2aadee4318de75cc8f2f8db1f8f004904160c39
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2024-04-03 13:03:26 -0400

    Create student.java

---
+++ Homework 7/student.java
@@ -0,0 +1,27 @@
+/*
+for this problem you need to:
+1. create 5 different students in your main
+2. create a method in this file that returns if they're an honor roll student or not (gpa > 3.6) then
+check each of the students in main
+3. create a method to see if they get "free lunch" (this is a weird school, every day they generate a random student id
+and see if any students match that id, if they do they get free lunch that day, yay!) compare a randomly generated id and the
+id of the student to print the appropriate response.
+ */
+public class student{
+    String name;
+    String year;
+    double gpa;
+    int id;
+    public student(String n, String y, double g, int i){
+        name = n;
+        year = y;
+        gpa = g;
+        id = i;
+    }
+    
+    //new method
+
+    
+    //new method
+
+}
