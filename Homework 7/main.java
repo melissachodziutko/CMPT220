2a4f7822be01af943d6f7c47adefee940975f97c
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2024-04-03 13:04:31 -0400

    Update and rename classdebug.java to main.java

---
+++ Homework 7/main.java
@@ -0,0 +1,16 @@
+// I gave a working example of a student!
+public class Main {
+    public static void main(String[] args){
+
+            student MelC = new student("Melissa","Senior",3.4,1343);
+
+    }
+}
+
+
+
+
+
+
+
+
