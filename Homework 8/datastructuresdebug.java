b5ebea3e90454a0b1fb6d59dbc355fac580d614e
Melissa Chodziutko <11694558+melissachodziutko@users.noreply.github.com>
2024-04-08 12:20:47 -0400

    Create datastructuresdebug.java

---
+++ Homework 8/datastructuresdebug.java
@@ -0,0 +1,41 @@
+/*
+Remember that stacks/queues/linkedlists all work super similarly to arraylists, so i bet you $10 you could reference earlier HW if you get stuck....
+
+1. Take the stack I created and fine the maximum and minimum value
+2. Find out how many elements were in the stack
+
+3. Find the middle value of the linkedlist I created
+
+4. Implement a real life example of any of the structures and do at least 3 operations to it
+*/
+
+import java.util.Stack;
+
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack<Integer> stax = new Stack<>();
+        
+        stax.push(70);
+        stax.push(34);
+        stax.push(807);
+        stax.push(61);
+        stax.push(28);
+        stax.push(563);
+        stax.push(492);
+        stax.push(386);
+        
+        
+        
+        LinkedList<Integer> linky = new LinkedList<>();
+
+        linky.add(10);
+        linky.add(20);
+        linky.add(30);
+        linky.add(40);
+        linky.add(50);
+
+
+    }
+}
