9b3d7a0d3288bf5f41d3061808d7eaa4ffec506b
Melissa Rodriguez <167561736+melissarodriguezz@users.noreply.github.com>
2024-09-05 18:16:11 -0400

    hw1

--- Homework 1/Debug.java
+++ Homework 1/Debug.java
@@ -1,7 +1,7 @@
 //always start with importing our scanner so we can use it!
 import java.util.Scanner;
 
-public class Main {
+public class Debug {
     public static void main(String[] args) {
         //I've already set up a scanner with the name 'sc' for you!
         Scanner sc = new Scanner(System.in);
@@ -20,14 +20,17 @@ public class Main {
 
         //finish this one for me by receiving and printing the user's age back to them
         System.out.println("How old are you? ");
+        String age = sc.nextLine();
+        System.out.println("You are " + age + " years old.");
 
 
 
 
-        
+
         //can you do some math for me? take these variable and print out the sum of them!
         int num1 = 10;
         int num2 = 37;
+        System.out.println(num1 + num2);
         //?
 
         
---
+++ Homework 1/test.java
@@ -0,0 +1,16 @@
+public class test {
+    public static void main(String[] args) {
+       System.out.println("Hello World");
+    System.out.println(12); 
+
+    String name = "Melissa";
+    System.out.println(name);
+    System.out.println(name);
+
+    Integer age = 20;
+    System.out.println(name + " is " + age + " years old.");
+
+    }
+
+}
+
